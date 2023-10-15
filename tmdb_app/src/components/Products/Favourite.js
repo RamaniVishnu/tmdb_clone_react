37eb727e8f3d50b19edaebae09d3a197f1eb9cde
Ramani V <86917886+RamaniVishnu@users.noreply.github.com>
2023-10-15 20:21:10 +0530

    Update Favourite.js

--- tmdb_app/src/components/Products/Favourite.js
+++ tmdb_app/src/components/Products/Favourite.js
@@ -6,23 +6,23 @@ import './Favourite.scss'
 import { useDispatch } from "react-redux";
 import { removeItem } from "../../store/favouriteSlice";
 import CloseIcon from '@mui/icons-material/Close';
+import Wishlist from '../../assets/Wishlist.png';
 
 function Favourite(){
     const data = useSelector((store)=> store.favouriteSlice.items);
     const dispatch = useDispatch()
-    if(!data) return null;
     return (
         <>
         <Header />
         <div className="favourites_data">
-        {data.map((data,index)=><div><div key={index} className="favourite_card"><img className="favourite_img" src={IMAGE_PATH+data}></img></div><button style={{background: "white",
+        {data.length > 0 ? data.map((data,index)=><div><div key={index} className="favourite_card"><img className="favourite_img" src={IMAGE_PATH+data}></img></div><button style={{background: "white",
     border: "none",
     display: "flex",
     margin: "5px auto",
     padding: "5px"}} onClick={()=>dispatch(removeItem(data))}><CloseIcon 
         style={{backgroundColor: "white",borderRadius: "50%",width: "26px",border: "1px solid #959595",fontSize: "25px"}}/><p style={{margin: "0 3px 0 5px",
     color: "#959595",
-    fontSize: "20px"}}>Remove</p></button></div>)}
+    fontSize: "20px"}}>Remove</p></button></div>): <div style={{height:"500px",justifyContent:"center",display:"flex",margin:"auto"}}><img src={Wishlist} alt="imaaaa" /></div>}
         </div>
         <Footer />
         </>
