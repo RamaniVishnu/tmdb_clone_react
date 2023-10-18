58428a570b18ef392bb57a910f4cf4d561ca7158
Ramani V <86917886+RamaniVishnu@users.noreply.github.com>
2023-10-18 15:58:11 +0530

    Update Constant.js
    Commented the youtube api

--- tmdb_app/src/utils/Constant.js
+++ tmdb_app/src/utils/Constant.js
@@ -1,3 +1,5 @@
 export const IMAGE_PATH = 'https://www.themoviedb.org/t/p/w220_and_h330_face/';
 
-export const YOUTUBE_SEARCH_API ="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
+//the youtube search api was used as a demo for search bar and it is replaced with tmdb search api.note: the tmdb api is bit slow compared to
+//the youtube search api and so there will be a delay in display of results
+// export const YOUTUBE_SEARCH_API ="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
