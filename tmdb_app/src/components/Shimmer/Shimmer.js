e60691fecffbc772847a0af734bf5c79fee645fc
Ramani V <86917886+RamaniVishnu@users.noreply.github.com>
2023-10-20 12:44:06 +0530

    Update Shimmer.js
    removed banner and link and added additional card

--- tmdb_app/src/components/Shimmer/Shimmer.js
+++ tmdb_app/src/components/Shimmer/Shimmer.js
@@ -4,10 +4,7 @@ import './Shimmer.scss'
 function Shimmer() {
   return (
     <div className='shimmer_layer'>
-    <div className='banner'>
-    </div>
-    <div className='links'></div>
-    {Array(10).fill('').map((e)=><div className='cards'></div>)}
+    {Array(22).fill('').map((e)=><div className='cards'></div>)}
     </div>
   )
 }
