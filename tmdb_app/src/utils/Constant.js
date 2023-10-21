ee4b5ccc6be8f1a0f93b4681f283f85186b54be6
Ramani V <86917886+RamaniVishnu@users.noreply.github.com>
2023-10-21 17:59:38 +0530

    Delete tmdb_app directory

--- tmdb_app/src/utils/Constant.js
+++
@@ -1,5 +0,0 @@
-export const IMAGE_PATH = 'https://www.themoviedb.org/t/p/w220_and_h330_face/';
-
-//the youtube search api was used as a demo for search bar and it is replaced with tmdb search api.note: the tmdb api is bit slow compared to
-//the youtube search api and so there will be a delay in display of results
-// export const YOUTUBE_SEARCH_API ="https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=";
