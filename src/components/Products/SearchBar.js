5bc2f8fb3b3ec9fb596612cb2ed4fd850682a02b
Ramani V <86917886+RamaniVishnu@users.noreply.github.com>
2023-12-31 19:04:41 +0530

    Update SearchBar.js

--- src/components/Products/SearchBar.js
+++ src/components/Products/SearchBar.js
@@ -57,7 +57,7 @@ export default function SearchBar(props){
             } else {
             getSearchSuggestion()
             }
-        },200);
+        },100);
 
         return ()=>{
             setshowSuggestion(false);
